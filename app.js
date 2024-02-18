7c4b96785d4f98d839d2f0a3c324399dede7cc5e
Benito Mendez <benedictwheatley@gmail.com>
2024-02-18 00:28:08 -0600

    encriptador primera version

--- app.js
+++ app.js
@@ -1,23 +1,66 @@
 
-
 function encriptar(){
-    let texto_a_Encriptar = texto_a_Procesar.value;
-    console.log(texto_a_Encriptar);
-    texto_a_Procesado.style="background:#FFFFFF;";
-    copiar_click.style.visibility = 'visible';
-    mensaje_global_id.style.visibility = 'hidden';
-    texto_a_Procesado.value = texto_a_Encriptar;
-
-    //let variable2 = document.getElementById("texto_a_Procesado");
-    //variable.style.visibility = "hidden";
-    //document.querySelector('#imagenmon').setAttribute('hidden','true');
-    
-
-  //let array = texto_a_Encriptar.value.replace(/\s+/g, ' ').split(' ').filter((e) => e.length > 0);
+  mostarContenido();
   
- //console.log(array);
-}
+  let texto = texto_a_encriptar.value;
+ 
+  for( i = 0; i < texto.length; i = i + 1){
+    let j = i + 1;
+    let v = (texto.substring(i,j)).charCodeAt(0) + 98;
+    texto_encriptado.value = texto_encriptado.value + String.fromCharCode(v);
+  }
+  console.log(texto_encriptado.value);
+  limpiarCaja();
+  
+  }
 
 function desencriptar(){
+  let texto = document.getElementById('texto_a_encriptar').value
+  texto_encriptado.value = '';
+  for( i = 0; i < texto.length; i = i + 1){
+    let j = i + 1;
+    let v = (texto.substring(i,j)).charCodeAt(0) - 98;
+    texto_encriptado.value = texto_encriptado.value + String.fromCharCode(v);
+  }
+    mostarContenido();
+    limpiarCaja();
+    
+}
+
+function copiar(){
+  navigator.clipboard.writeText(texto_encriptado.value);
+  alert("Mensaje copiado");
+  location.reload();
+}
+
+function limpiarCaja() {
+    texto_a_encriptar.value = '';
 
 }
+function mostarContenido(){
+  texto_encriptado.style = "background-image: none;";
+  copiar_click.style = 'visibility: visible;';
+  texto_bajoimagen.style = 'visibility: hidden;';
+}
+function ocultarContenido(){
+  texto_encriptado.style = "background-image: url('/imagenes/Muñeco.png');";
+  copiar_click.style = 'visibility: hidden;';
+  texto_bajoimagen.style = 'visibility: visible;';
+}
+
+/*function redimensionar_caja(){
+  if (window.matchMedia("(max-width: 768px)").matches = true){
+    texto_encriptado.style = "height: 180px;";
+  }else{
+    texto_encriptado.style = "height: 781px;";
+  }
+  if (window.matchMedia("(max-width: 375px)").matches = true){
+    texto_encriptado.style = "height: 432px;";
+    }else{
+      texto_encriptado.style = "height: 781px;"
+    }
+}*/
+function condicionesIniciales(){
+  ocultarContenido();
+}
+//window.onresize = redimensionar_caja;
