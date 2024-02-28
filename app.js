6b0a8d29a96cf0c856b7dd97883b7b0ed1885564
Benito Mendez <benedictwheatley@gmail.com>
2024-02-27 18:15:02 -0600

    cambios en estilo

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 
-const texto_Resultado = document.getElementById('texto_encriptado');
-
 function encriptar(){
-  texto_Resultado.innerHTML = '';
-  mostarContenido();
-  
+  limpiarCaja2();
   let texto = texto_a_encriptar.value;
   for( i = 0; i < texto.length; i = i + 1){
     let j = i + 1;
     let v = (texto.substring(i,j)).charCodeAt(0) + (texto.length);
-    texto_Resultado.innerHTML = texto_Resultado.innerHTML + String.fromCharCode(v);
+    texto_encriptado.value = texto_encriptado.value  + String.fromCharCode(v);
+    texto_encriptado_tablet.value = texto_encriptado.value;
+    texto_encriptado_celular.value = texto_encriptado.value;
   }
+  mostarContenido();
   limpiarCaja();
   
   }
 
 function desencriptar(){
-  let texto = document.getElementById('texto_a_encriptar').value
-  texto_Resultado.innerHTML = '';
+  limpiarCaja2();
+  let texto = texto_a_encriptar.value;
   for( i = 0; i < texto.length; i = i + 1){
     let j = i + 1;
     let v = (texto.substring(i,j)).charCodeAt(0) - (texto.length);
-    texto_Resultado.innerHTML = texto_Resultado.innerHTML + String.fromCharCode(v);
+    texto_encriptado.value = texto_encriptado.value  + String.fromCharCode(v);
+    texto_encriptado_tablet.value = texto_encriptado.value;
+    texto_encriptado_celular.value = texto_encriptado.value;
   }
     mostarContenido();
     limpiarCaja();
@@ -29,22 +30,33 @@ function desencriptar(){
 }
 
 function copiar(){
-  navigator.clipboard.writeText(texto_Resultado.innerHTML);
+  navigator.clipboard.writeText(texto_encriptado.value);
   const storage = document.createElement('textarea');
-  storage.value = texto_Resultado.innerHTML;
-  texto_Resultado.appendChild(storage);
+  storage.value = texto_encriptado.value;
+  texto_encriptado.value.appendChild(storage);
   storage.select();
   storage.setSelectionRange(0, 99999);
   document.execCommand('copy');
-  texto_Resultado.removeChild(storage);
+  texto_encriptado.value.removeChild(storage);
   alert("Mensaje copiado");
 }
 function mostarContenido(){
   cuadro_Texto_Encriptado.style = 'background-image: none;';
-  texto_bajoimagen.style = 'display: none';
-  copiar_click.style = 'display: grid';
+  texto_bajoimagen.style = 'display: none;';
+  texto_bajoimagen_tablet.style = 'display: none;';
+  texto_bajoimagen_celular.style = 'display: none;';
+  copiar_click.style = 'display: grid;';
+  copiar_click_tablet.style = 'display: grid;';
+  copiar_click_celular.style = 'display: grid;';
+  cuadro_Texto_Encriptado_tablet.style = "height: 20%; transition: 1s";
+  cuadro_Texto_Encriptado_celular.style = "height: 70%; transition: 1s;";
 }
 function limpiarCaja() {
     texto_a_encriptar.value = '';
 
+}
+function limpiarCaja2(){
+  texto_encriptado.value = '';
+    texto_encriptado_tablet.value = '';
+    texto_encriptado_celular.value = '';
 }
